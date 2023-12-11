78daeb997487335f4eeb210b4228b084157bb533
cmcoke <minoru.cm@gmail.com>
2023-12-11 18:18:54 -0500

    created the shopping cart modal and implemented stripe.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ShoppingBag } from "lucide-react";
+import { useShoppingCart } from "use-shopping-cart";
 
 const links = [
   { name: "Home", href: "/" },
@@ -12,9 +13,9 @@ const links = [
   { name: "Teens", href: "/Teens" }
 ];
 
-export default function Navbar() {
+const Navbar = () => {
   const pathname = usePathname();
-
+  const { handleCartClick } = useShoppingCart();
   return (
     <header className="mb-8 border-b">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
@@ -41,7 +42,7 @@ export default function Navbar() {
         </nav>
 
         <div className="flex divide-x border-r sm:border-l">
-          <Button variant={"outline"} onClick={() => {}} className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
+          <Button variant={"outline"} onClick={() => handleCartClick()} className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
             <ShoppingBag />
             <span className="hidden text-xs font-semibold text-gray-500 sm:block">Cart</span>
           </Button>
@@ -49,4 +50,5 @@ export default function Navbar() {
       </div>
     </header>
   );
-}
+};
+export default Navbar;
